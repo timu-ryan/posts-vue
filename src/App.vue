bce0dd1982e9a84a6dbed9e1c28383faac74e5e5
Tim Gafarov <timu.ryan@yandex.ru>
2025-09-27 15:55:20 +0300

    feat: basic likes/dislikes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    It works!
+    <div>
+      <button @click="addLike">like</button>
+      <button @click="addDislike">dislike</button>
+    </div>
+    <div>likes: <strong>{{ likes }}</strong></div>
+    <div>dislikes: <strong>{{ dislikes }}</strong></div>
+  </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        likes: 0,
+        dislikes: 0,
+      }
+    },
+    methods: {
+      addLike() {
+        this.likes += 1;
+      },
+      addDislike() {
+        this.dislikes += 1;
+      },
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
