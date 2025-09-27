c4a84aa91bdf40dce649f3fc74b97a37d100f674
Tim Gafarov <timu.ryan@yandex.ru>
2025-09-27 16:50:17 +0300

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    It works!
-    <div>
-      <button @click="addLike">like</button>
-      <button @click="addDislike">dislike</button>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Post creating</h4>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        type="text"
+        placeholder="Name"
+        class="input"
+      />
+      <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+        type="text"
+        placeholder="Content"
+        class="input"
+      />
+      <button
+        class="btn"
+        @click="createPost"
+      >
+        Create
+      </button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Name: </strong>{{ post.title }}</div>
+      <div><strong>Content: </strong>{{ post.body }}</div>
     </div>
-    <div>likes: <strong>{{ likes }}</strong></div>
-    <div>dislikes: <strong>{{ dislikes }}</strong></div>
   </div>
 </template>
 
@@ -14,21 +34,68 @@
   export default {
     data() {
       return {
-        likes: 0,
-        dislikes: 0,
+        posts: [
+          {id: 1, title: 'JavaScript', body: 'a programming language'},
+          {id: 2, title: 'Python', body: 'a programming language'},
+          {id: 3, title: 'Go', body: 'a programming language'},
+          {id: 4, title: 'C++', body: 'a programming language'},
+        ],
+        title: '',
+        body: '',
       }
     },
     methods: {
-      addLike() {
-        this.likes += 1;
-      },
-      addDislike() {
-        this.dislikes += 1;
+      createPost() {
+        const newPost = {
+          id: Date.now(),
+          title: this.title,
+          body: this.body,
+        }
+        this.posts.push(newPost)
+        this.title = ''
+        this.body = ''
       },
     }
   }
 </script>
 
 <style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
+  .app {
+    margin: 0 10px;
+  }
+
+  .post {
+    padding: 15px;
+    border: 1px solid gray;
+    margin-top: 10px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    border-bottom: 1px solid gray;
+  }
+
+  .btn {
+    margin-top: 10px;
+    margin-bottom: 40px;
+    padding: 10px;
+    background: transparent;
+    border: 1px solid gray;
+
+    align-self: end;
+  }
+
+  .input {
+    border: 1px solid gray;
+    padding: 10px;
+    width: 100%;
+    margin-top: 10px;
+  }
 </style>
